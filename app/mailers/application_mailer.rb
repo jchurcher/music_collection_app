621b7a723a0603285bc5dee5d3c8d0ed117719b0
James <jc01901@surrey.ac.uk>
2020-12-05 18:59:47 +0000

    Added mailer, improved views

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default to: 'from@musicCollection.com', from: 'from@musicCollection.com'
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'home#home'
 
   get 'contact', to: 'home#contact'
+  post 'request_contact', to: 'home#request_contact'
 end
